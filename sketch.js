d35b9797703c9309af61b28df74c2a92039b9263
Ilya Petrov <ilyapetrov.mosobl@gmail.com>
2018-12-18 19:42:30 +0300

    added some nice bodybuilders OUCH THAT HURT

--- sketch.js
+++ sketch.js
@@ -2,9 +2,15 @@
 var stars = [];
 var count = 600;
 
+function preload(){
+  billyImg = loadImage('billyChristmas.jpg');
+  gachi = loadSound('gachiSong.mp3');
+}
+
 function setup() {
-  createCanvas(1000, 700);
+  createCanvas(window.innerWidth, window.innerHeight);
   frameRate(30);
+
   var counter = 0;
   var x       = random(0, width);
   var y       = random(0, height);
@@ -21,6 +27,10 @@ function setup() {
 
 function draw() {
   background(0);
+  if (gachi.isPlaying()){}
+  else gachi.play();
+  image(billyImg, width/2-billyImg.width/2,height/2-billyImg.height/2)
+
   var counter = 0;
     while (counter < count){
       stars[counter].draw();
@@ -44,5 +54,4 @@ Star.prototype.draw = function () {
     var speed = map(this.brightness, 3, 10, 1, 4);
     this.y = this.y + speed;
     if(this.y > height) this.y = -30;
-
 }
